8f420e56f371e6574c3892ec7a2cec90486812f0
Jordi Schaefer <jschaefer21@gmail.com>
2022-07-19 18:03:04 +0200

    hide personal weather api key

--- project/api/src/logic/retrieveTemperature.js
+++ project/api/src/logic/retrieveTemperature.js
@@ -1,12 +1,14 @@
 const axios = require('axios');
 const { NotFoundError } = require('errors');
 const { validatePosition } = require('validators')
+require('dotenv').config()
+const { env: {WEATHER_KEY}} = process
 
 function retrieveTemperature(latitude, longitude) {
   validatePosition([latitude, longitude])
 
 return axios
-  .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=93a177915b460f1e6b2b2548a8b673cc`)
+  .get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&APPID=${WEATHER_KEY}`)
   .then(res => {
     if(res.status === 200){
         const data = res.data
